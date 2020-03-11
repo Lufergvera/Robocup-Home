c78c227d02627ff76cd05e59eb6aacab5d4e57da
Paul Vazquez <pev@live.com.mx>
2020-03-10 22:13:57 -0600

    add pixel finding

--- catkin_home/src/my_pcl_tutorial/include/ProcessingCloud.h
+++ catkin_home/src/my_pcl_tutorial/include/ProcessingCloud.h
@@ -21,29 +21,29 @@ pcl::PointCloud<pcl::PointXYZ>::Ptr CloudFinal(new pcl::PointCloud<pcl::PointXYZ
 pcl::PointCloud<pcl::PointXYZRGB>::Ptr inputcloud_RGB(new pcl::PointCloud<pcl::PointXYZRGB>);
 pcl::PointCloud<pcl::PointXYZ>::Ptr inputcloud_XYZ(new pcl::PointCloud<pcl::PointXYZ>);
 
-pcl::PointCloud<pcl::PointXYZRGB>::Ptr BiggestCluster(const pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud, double tolerance, bool& flag) {
+/*pcl::PointCloud<pcl::PointXYZ>::Ptr BiggestCluster(const pcl::PointCloud<pcl::PointXYZ>::Ptr cloud, double tolerance, bool& flag) {
 	
 	int i = 0;
 	// Creating the KdTree object for the search method of the extraction
-	pcl::search::KdTree<pcl::PointXYZRGB>::Ptr tree(new pcl::search::KdTree<pcl::PointXYZRGB>);
+	pcl::search::KdTree<pcl::PointXYZ>::Ptr tree(new pcl::search::KdTree<pcl::PointXYZ>);
 	tree->setInputCloud(cloud);
 
 	//Set parameters for the clustering
 	std::vector<pcl::PointIndices> cluster_indices;
-	pcl::EuclideanClusterExtraction<pcl::PointXYZRGB> ec;
+	pcl::EuclideanClusterExtraction<pcl::PointXYZ> ec;
 	ec.setClusterTolerance(tolerance); // 5mm
-	ec.setMinClusterSize(100);
+	ec.setMinClusterSize(500);
 	ec.setMaxClusterSize(25000);
 	ec.setSearchMethod(tree);
 	ec.setInputCloud(cloud);
 	ec.extract(cluster_indices);
 
 	std::vector<unsigned int> npoints;
-	std::vector<pcl::PointCloud<pcl::PointXYZRGB>::Ptr> clouds;
+	std::vector<pcl::PointCloud<pcl::PointXYZ>::Ptr> clouds;
 
 	for (std::vector<pcl::PointIndices>::const_iterator it = cluster_indices.begin(); it != cluster_indices.end(); ++it)
 	{
-		pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_cluster(new pcl::PointCloud<pcl::PointXYZRGB>);
+		pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_cluster(new pcl::PointCloud<pcl::PointXYZ>);
 		for (std::vector<int>::const_iterator pit = it->indices.begin(); pit != it->indices.end(); ++pit)
 			cloud_cluster->points.push_back(cloud->points[*pit]); //*
 		cloud_cluster->width = cloud_cluster->points.size();
@@ -57,6 +57,7 @@ pcl::PointCloud<pcl::PointXYZRGB>::Ptr BiggestCluster(const pcl::PointCloud<pcl:
 		clouds.push_back(cloud_cluster);
 
 	}
+
 	int maxindex;
 	int count = 0;
 	if (npoints.size() == 0) {
@@ -75,12 +76,8 @@ pcl::PointCloud<pcl::PointXYZRGB>::Ptr BiggestCluster(const pcl::PointCloud<pcl:
 			i++;
 		}
 	}
-	ROS_INFO_STREAM(clouds.size());
-	ROS_INFO_STREAM(clouds[0]->points.size());
-
-	return clouds [0];
 	return clouds[maxindex>0?maxindex:0];
-}
+}*/
 
 /*void PasstroughFilterXYZ(const pcl::PointCloud<pcl::PointXYZ>::Ptr& in, pcl::PointCloud<pcl::PointXYZ>::Ptr& out, double lowboxlimits[3], double upperboxlimits[3]) {
 	//Use pass through filters on the H, S and V channels to find points with same colour as the hand.
@@ -252,10 +249,10 @@ pcl::PointCloud<pcl::PointXYZRGB>::Ptr PlanarRANSAC(const pcl::PointCloud<pcl::P
 	pcl::ExtractIndices<pcl::PointXYZRGB> extract2;
 	pcl::PointCloud<pcl::PointXYZRGB>::Ptr outcloud_XYZ2(new pcl::PointCloud<pcl::PointXYZRGB>);
 
-	extract2.setInputCloud(outcloud_XYZ);
-	extract2.setIndices(inliers2);
-	extract2.setNegative(true);
-	extract2.filter(*outcloud_XYZ2);
+	extract.setInputCloud(outcloud_XYZ);
+	extract.setIndices(inliers2);
+	extract.setNegative(true);
+	extract.filter(*outcloud_XYZ2);
 
 	/*
 	// create a pcl object to hold the passthrough filtered results
@@ -271,11 +268,13 @@ pcl::PointCloud<pcl::PointXYZRGB>::Ptr PlanarRANSAC(const pcl::PointCloud<pcl::P
 	zPLANE = cloud->points[inliers->indices[0]].z;
 	pass.filter(*xyzCloudPtrPassthroughFiltered);
 	*/
-	return outcloud_XYZ2;
+	return outcloud_XYZ;
 }
 
 my_pcl_tutorial::SegmentedClustersArray Clustering(const pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud)
 {
+	my_pcl_tutorial::SegmentedClustersArray CloudClusters;
+	/*
 	// Create the KdTree object for the search method of the extraction
 	pcl::search::KdTree<pcl::PointXYZRGB>::Ptr tree(new pcl::search::KdTree<pcl::PointXYZRGB>);
 	tree->setInputCloud(cloud);
@@ -326,7 +325,7 @@ my_pcl_tutorial::SegmentedClustersArray Clustering(const pcl::PointCloud<pcl::Po
 		// add the cluster to the array message
 		//clusterData.cluster = output;
 		CloudClusters.clusters.push_back(output);
-	}
+	}*/
 	return CloudClusters;
 }
 
@@ -342,7 +341,7 @@ pcl::PointCloud<pcl::PointXYZRGB>::Ptr CenteredCluster(const pcl::PointCloud<pcl
 	std::vector<pcl::PointIndices> cluster_indices;
 	pcl::EuclideanClusterExtraction<pcl::PointXYZRGB> ec;
 	ec.setClusterTolerance(tolerance); // 5mm
-	ec.setMinClusterSize(100);
+	ec.setMinClusterSize(500);
 	ec.setMaxClusterSize(25000);
 	ec.setSearchMethod(tree);
 	ec.setInputCloud(cloud);
@@ -354,7 +353,7 @@ pcl::PointCloud<pcl::PointXYZRGB>::Ptr CenteredCluster(const pcl::PointCloud<pcl
 	std::vector<float[2]> centroids;
 
 	std::vector<float> euclideanvec;
-
+/*
 	for (std::vector<pcl::PointIndices>::const_iterator it = cluster_indices.begin(); it != cluster_indices.end(); ++it)
 	{
 		pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_cluster(new pcl::PointCloud<pcl::PointXYZRGB>);
@@ -374,10 +373,10 @@ pcl::PointCloud<pcl::PointXYZRGB>::Ptr CenteredCluster(const pcl::PointCloud<pcl
 			center3D[2] += cloud_cluster->points[k].y;
 		}
 		for (int i = 0; i < 3; i++)
-			center3D[i] = center3D[i] / (cloud_cluster->points.size());
+			A[i] = A[i] / k;
 
-		float xSqr = (centroidlabel[0] - center3D[0]);
-		float ySqr = (centroidlabel[1] - center3D[1]);
+		float xSqr = (x1 - x2);
+		float ySqr = (y1 - y2);
 
 		float dist = std::pow(xSqr, 2) + std::pow(xSqr, 2);
 
@@ -391,8 +390,7 @@ pcl::PointCloud<pcl::PointXYZRGB>::Ptr CenteredCluster(const pcl::PointCloud<pcl
 		//Save the clouds in a vector
 		clouds.push_back(cloud_cluster);
 	}
-	
-	ROS_INFO_STREAM(clouds.size());
+*/
 
 	int maxindex;
 	int count = 0;
@@ -418,7 +416,6 @@ pcl::PointCloud<pcl::PointXYZRGB>::Ptr CenteredCluster(const pcl::PointCloud<pcl
 			
 		}
 	}
-	return clouds[0];
 	return clouds[maxindex > 0 ? maxindex : 0];
 
 }
